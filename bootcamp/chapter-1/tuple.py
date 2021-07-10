411ef1aeb787b1acb396876c4f29a165b19f84eb
Hitanya Srivastava <srivastavahitanya04@gmail.com>
2021-07-10 20:42:31 +0530

    tuples

---
+++ bootcamp/chapter-1/tuple.py
@@ -0,0 +1,19 @@
+# tuple of strings
+
+my_data = ("hi", "how", "are", "you")
+print(my_data)
+
+# tuple of int, float, string
+
+my_data2 = (1, 5.6, 28, "Hitanya")
+print(my_data2)
+
+#tuple of string and list
+
+my_data3 = ("pen", [5, 6, 7])
+print(my_data3)
+
+# nested tuple
+
+my_data4 = ((2, 3, 4)(1, 2, 3, "Hitanya"))
+print(my_data4)
