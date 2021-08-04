02d9dec624778c6a51e007735834473201a0360b
hitanya <srivastavahitanya04@gmail.com>
2021-08-04 12:24:57 +0530

    functions

---
+++ bootcamp/chapter-1/practice-1/functions.py
@@ -0,0 +1,51 @@
+# function to check that x is odd or even
+
+def evenOdd(x):
+    if (x % 2 == 0)
+        print "even"
+
+    else:
+        print "odd"
+              
+# docstring
+
+def say_hello():
+    "Hitanya Srivastava"
+
+
+print (say_hello ._ _ doc_ _)                  
+     
+# return statement
+
+def square_value (num):
+
+    return num**3
+
+
+print(square_value(3))
+
+print(square_value(-9))
+
+# new referance
+
+def myFun(x):
+    x[0] = 10
+
+lst = [11, 12, 13, 14, 15]
+myFun(lst)
+
+print(lst)
+
+# prctice
+
+def swap(x ,y)
+    temp = y
+    y = x
+    x = temp
+
+x = 2
+y = 3
+swap(x, y)
+
+print(x)
+print(y)
